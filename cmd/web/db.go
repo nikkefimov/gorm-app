1be591081b129123015b5d5e70185f797fcfdd10
Nik Efimov <nikkefimov@gmail.com>
2024-12-19 23:33:24 +0100

    connect gorm

---
+++ cmd/web/db.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	dsn := os.Getenv("DSN")
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Error to connect DB: %v", err)
+	}
+}
