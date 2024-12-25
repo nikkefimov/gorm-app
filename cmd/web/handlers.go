a9061ad9cd343ce1f5be39f6e24845b9b0400de5
Nik Efimov <nikkefimov@gmail.com>
2024-12-25 02:29:06 +0100

    movie page handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 )
 
+// Movie create handler.
 func createMoviePage(w http.ResponseWriter, r *http.Request) {
 	session, _ := store.Get(r, "session")
 	userID, ok := session.Values["userID"].(uint)
@@ -33,4 +34,25 @@ func createMoviePage(w http.ResponseWriter, r *http.Request) {
 
 	tmpl, _ := template.ParseFiles("public/movie_create.html")
 	tmpl.Execute(w, nil)
+}
+
+// Movie page handler.
+func moviePage(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "session")
+	userID, ok := session.Values["userID"].(uint)
+	if !ok {
+		http.Redirect(w, r, "/home", http.StatusFound)
+		return
+	}
+
+	// Looking movie in the list.
+	var movies []Movie
+	if err := DB.Where("user_id = ?", userID).Find(&movies).Error; err != nil {
+		http.Error(w, "Error movies list",
+			http.StatusInternalServerError)
+		return
+	}
+
+	tmpl, _ := template.ParseFiles("template/movie_page.html")
+	tmpl.Execute(w, movies)
 }
